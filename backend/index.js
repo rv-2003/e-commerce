cc2dd1fe42ab3af903e645665e743005851c5fd6
rv-2003 <rohitvijay43273@gmail.com>
2026-02-11 17:22:43 +0530

    ready for deployment

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,12 @@ const port = process.env.PORT || 5000;
 connectDB();
 
 const app = express();
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
+// All unmatched routes return index.html
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
---
+++ vercel.js
@@ -0,0 +1,16 @@
+{
+  "projects": [
+    {
+      "src": "frontend/package.json",
+      "use": "@vercel/static-build",
+      "config": { "distDir": "dist" },
+      "name": "frontend"
+    },
+    {
+      "src": "backend/index.js",
+      "use": "@vercel/node",
+      "name": "backend"
+    }
+  ]
+}
+,
