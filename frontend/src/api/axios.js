209a97fa7511aa2dd0ccd585ffbaab9fb775f4d7
rv-2003 <rohitvijay43273@gmail.com>
2026-02-12 10:10:31 +0530

    fixed error

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,10 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.PROD
-    ? import.meta.env.VITE_API_URL
-    : "http://localhost:5000/api",
-  withCredentials: true, // ✅ send cookies
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+  withCredentials: true, // sends cookies
 });
 
 // REMOVE Authorization header since backend uses cookies
